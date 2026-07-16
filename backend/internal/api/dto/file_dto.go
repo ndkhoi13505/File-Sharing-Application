c7fc0e5e65738a6a74e0cbf08be9bad07ac759df
ndkhoi13505 <ndkhoi13505@gmail.com>
2026-07-16 20:50:18 +0700

    Update file_dto.go

--- backend/internal/api/dto/file_dto.go
+++ backend/internal/api/dto/file_dto.go
@@ -4,8 +4,8 @@ import "time"
 
 type UploadRequest struct {
 	IsPublic				bool		`form:"isPublic"`
-	FileNameForValidation	string		`form:"file_validation_placeholder" validate:"file_ext=pdf jpg png txt"`
-	Password				*string		`form:"password" validate:"omitempty,min=6"`
+	FileNameForValidation	string		`form:"file_validation_placeholder"`
+	Password				*string		`form:"password" validate:"omitempty,min=8"`
 	AvailableFrom			*time.Time	`form:"availableFrom" time_format:"2006-01-02T15:04:05Z"`
 	AvailableTo				*time.Time	`form:"availableTo" time_format:"2006-01-02T15:04:05Z"`
 	SharedWith				[]string	`form:"sharedWith"`
